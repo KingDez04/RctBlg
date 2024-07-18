bbaac14d5925f4fffa6e96a4c1419064001ea2bb
Ademola Ademeso <desmondademeso@gmail.com>
2024-07-18 22:41:49 +0100

    Added deployment test resource

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -3,11 +3,11 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const { data: blog, error, isPending } = useFetch('https://my-json-server.typicode.com/KingDez04/db/blogs/' + id);
     const navigate = useNavigate();
 
     const handleClick = () => {
-        fetch('http://localhost:8000/blogs/' + blog.id, {
+        fetch('https://my-json-server.typicode.com/KingDez04/db/blogs/' + blog.id, {
             method: 'DELETE'
         }).then(() => {
             navigate('/');
