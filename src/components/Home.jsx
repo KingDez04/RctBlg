f575b40a298485ad6d9b5f8797c903c89b4af1c4
Ademola Ademeso <desmondademeso@gmail.com>
2024-07-18 22:35:00 +0100

    Added jsonplaceholder resource to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import BlogList from './BlogList';
 import useFetch from './useFetch';
 const Home = () => {
-    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+    const { data: blogs, isPending, error } = useFetch('https://my-json-server.typicode.com/KingDez04/db/blogs');
     
     return (
         <div className="home">
